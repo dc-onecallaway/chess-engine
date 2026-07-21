c351258b15b5030de0dbc3ba3be8c614aa8725f3
dc-onecallaway <chauhandeepak21103@gmail.com>
2026-07-21 21:08:51 +0530

    feat: refactor move generators

--- src/MoveGenerator.cpp
+++ src/MoveGenerator.cpp
@@ -3,106 +3,61 @@
 
 void MoveGenerator::generateKnightMoves(const Board &board, std::vector<Move> &moves)
 {
+    bool whiteToMove = board.isWhiteToMove();
+    Piece knight = whiteToMove ? Piece::WhiteKnight : Piece::BlackKnight;
 
-    if (board.isWhiteToMove())
+    uint64_t ownOccupied = whiteToMove ? board.getWhiteOccupancy() : board.getBlackOccupancy();
+    uint64_t enemyOccupied = whiteToMove ? board.getBlackOccupancy() : board.getWhiteOccupancy();
+
+    uint64_t knights = board.getPieceBitboard(knight);
+
+    while (knights)
     {
-        uint64_t knights = board.getPieceBitboard(Piece::WhiteKnight);
+        int square = __builtin_ctzll(knights);
+        uint64_t attacks = AttackTables::knightAttacks[square];
+        uint64_t pseudoAttacks = (attacks & ~ownOccupied);
 
-        while (knights)
+        while (pseudoAttacks)
         {
-            int square = __builtin_ctzll(knights);
-            uint64_t attacks = AttackTables::knightAttacks[square];
-            uint64_t occupied = board.getWhiteOccupancy();
-            uint64_t pseudoAttacks = (attacks & ~occupied);
+            int attackSquare = __builtin_ctzll(pseudoAttacks);
 
-            while (pseudoAttacks)
-            {
-                int attackSquare = __builtin_ctzll(pseudoAttacks);
+            MoveType type =
+                (enemyOccupied & (1ULL << attackSquare))
+                    ? MoveType::Capture
+                    : MoveType::Quiet;
 
-                MoveType type =
-                    (board.getBlackOccupancy() & (1ULL << attackSquare))
-                        ? MoveType::Capture
-                        : MoveType::Quiet;
-
-                moves.emplace_back(square, attackSquare, type);
-                pseudoAttacks &= (pseudoAttacks - 1);
-            }
-
-            knights &= (knights - 1);
+            moves.emplace_back(square, attackSquare, type);
+            pseudoAttacks &= (pseudoAttacks - 1);
         }
-    }
-    else
-    {
-        uint64_t knights = board.getPieceBitboard(Piece::BlackKnight);
 
-        while (knights)
-        {
-            int square = __builtin_ctzll(knights);
-            uint64_t attacks = AttackTables::knightAttacks[square];
-            uint64_t occupied = board.getBlackOccupancy();
-            uint64_t pseudoAttacks = (attacks & ~occupied);
-
-            while (pseudoAttacks)
-            {
-                int attackSquare = __builtin_ctzll(pseudoAttacks);
-
-                MoveType type =
-                    (board.getWhiteOccupancy() & (1ULL << attackSquare))
-                        ? MoveType::Capture
-                        : MoveType::Quiet;
-
-                moves.emplace_back(square, attackSquare, type);
-                pseudoAttacks &= (pseudoAttacks - 1);
-            }
-
-            knights &= (knights - 1);
-        }
+        knights &= (knights - 1);
     }
 }
 
 void MoveGenerator::generateKingMoves(const Board &board, std::vector<Move> &moves)
 {
+    bool whiteToMove = board.isWhiteToMove();
+    Piece kingType = whiteToMove ? Piece::WhiteKing : Piece::BlackKing;
 
-    if (board.isWhiteToMove())
+    uint64_t ownOccupied = whiteToMove ? board.getWhiteOccupancy() : board.getBlackOccupancy();
+    uint64_t enemyOccupied = whiteToMove ? board.getBlackOccupancy() : board.getWhiteOccupancy();
+
+    uint64_t king = board.getPieceBitboard(kingType);
+
+    int square = __builtin_ctzll(king);
+    uint64_t attacks = AttackTables::kingAttacks[square];
+    uint64_t pseudoAttacks = (attacks & ~ownOccupied);
+
+    while (pseudoAttacks)
     {
-        uint64_t king = board.getPieceBitboard(Piece::WhiteKing);
-        int square = __builtin_ctzll(king);
-        uint64_t attacks = AttackTables::kingAttacks[square];
-        uint64_t occupied = board.getWhiteOccupancy();
-        uint64_t pseudoAttacks = (attacks & ~occupied);
+        int attackSquare = __builtin_ctzll(pseudoAttacks);
 
-        while (pseudoAttacks)
-        {
-            int attackSquare = __builtin_ctzll(pseudoAttacks);
+        MoveType type =
+            (enemyOccupied & (1ULL << attackSquare))
+                ? MoveType::Capture
+                : MoveType::Quiet;
 
-            MoveType type =
-                (board.getBlackOccupancy() & (1ULL << attackSquare))
-                    ? MoveType::Capture
-                    : MoveType::Quiet;
-
-            moves.emplace_back(square, attackSquare, type);
-            pseudoAttacks &= (pseudoAttacks - 1);
-        }
-    }
-    else
-    {
-        uint64_t king = board.getPieceBitboard(Piece::BlackKing);
-        int square = __builtin_ctzll(king);
-        uint64_t attacks = AttackTables::kingAttacks[square];
-        uint64_t occupied = board.getBlackOccupancy();
-        uint64_t pseudoAttacks = (attacks & ~occupied);
-
-        while (pseudoAttacks)
-        {
-            int attackSquare = __builtin_ctzll(pseudoAttacks);
-
-            MoveType type =
-                (board.getWhiteOccupancy() & (1ULL << attackSquare))
-                    ? MoveType::Capture
-                    : MoveType::Quiet;
-
-            moves.emplace_back(square, attackSquare, type);
-            pseudoAttacks &= (pseudoAttacks - 1);
-        }
+        moves.emplace_back(square, attackSquare, type);
+        pseudoAttacks &= (pseudoAttacks - 1);
     }
 }
